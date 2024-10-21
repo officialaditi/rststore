b354f1cc86b47c27482a0e06639b5fe4a06f7efb
aditi <aditigaikwad0110@gmail.com>
2024-10-21 17:51:32 +0530

    step 14 completed: Improve console log message with colors module DONE

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import colors from 'colors';
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.bgMagenta);
   } catch (err) {
-    console.err(`Error:- ${err.message}`);
+    console.err(`Error:- ${err.message}`.bgRed);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+  );
 });
