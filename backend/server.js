b5b66a8f47ab95d53385afcb46cf3791a5055661
aditi <aditigaikwad0110@gmail.com>
2024-10-21 17:41:19 +0530

    step 10 completed: Setup of enviroment variable DONE

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 import products from "./data/products.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((prod) => prod._id === req.params.id);
-    res.json(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((prod) => prod._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(3000, () => {
-  console.log("Server Started on port 3000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
