530fb7be84d9dfcafafb88e7acc5e19d346ed569
aditi <aditigaikwad0110@gmail.com>
2024-10-22 12:42:59 +0530

    step 28 completed: 'add to cart' functionality in redux DONE

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,25 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { thunk } from "redux-thunk";
-import { allProductReducer, singleProductReducer } from "./redux/reducers/productReducer";
+import {
+  allProductReducer,
+  singleProductReducer,
+} from "./redux/reducers/productReducer";
+import { cartReducer } from "./redux/reducers/cartReducer";
 
 const reducers = combineReducers({
   allProductList: allProductReducer,
   singleProductDetail: singleProductReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
