58af9bd81c2a5ff4693f053910414b142837f1c3
aditi <aditigaikwad0110@gmail.com>
2024-10-22 06:52:55 +0530

    step 21 completed: Introduction and setup of store redux DONE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
