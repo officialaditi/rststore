6730f758a30f91f28764c4e5b5fdfff6d5c22e5d
aditi <aditigaikwad0110@gmail.com>
2024-10-23 15:49:19 +0530

    changes

--- frontend/src/Screens/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen.jsx
@@ -34,7 +34,7 @@ const LoginScreen = () => {
   const { loading, error, userInfo } = userLogin;
 
   useEffect(() => {
-    console.log('userInfo', userInfo)
+    console.log("userInfo", userInfo);
     if (userInfo) {
       navigate(redirect);
     }
